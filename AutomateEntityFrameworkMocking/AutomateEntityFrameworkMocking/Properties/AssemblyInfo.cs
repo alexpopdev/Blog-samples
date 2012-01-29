042ff93f3def96c02e73b120cb959c6445681d79
Alex Pop <palexandru@gmail.com>
2012-01-29 20:54:35 +0000

    initial solution commit

---
+++ AutomateEntityFrameworkMocking/AutomateEntityFrameworkMocking/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyInfo")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d162f393-9af6-4b3b-a871-e09c400c71c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutomateEntityFrameworkMocking/AutomateEntityFrameworkMocking/TestFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace AutomateEntityFrameworkMocking
+{
+    [TestFixture]
+    public class TestFixture1
+    {
+        [Test]
+        public void Customers_Count_GreaterThanZero()
+        {
+            int customersCount = 0;
+            using (var dataContext = new NorthwindEntities())
+            {
+                customersCount = dataContext.Customers.Count();
+            }
+            Assert.That(customersCount, Is.GreaterThan(0));
+        }
+
+        // This test fail for example, replace result or delete this test to see all tests pass
+        [Test]
+        public void TestFault()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
