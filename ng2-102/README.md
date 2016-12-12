a0a1b62e1cb76c226f17b5112ce84599eaeef4fb
Alex Pop <palexandru@gmail.com>
2016-12-12 20:55:56 +0000

    Clone ng2-101 example as the base for ng-102

---
+++ ng2-102/README.md
@@ -0,0 +1,6 @@
+# ng2-102
+An Angular 2 102 example for parent and child components with directives
+
+Prerequisites: Node.js and TypeScript
+
+Execute `npm install` and `npm run start` to install the required dependencies and see the example in action.
---
+++ ng2-102/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { ChildComponent } from './child.component';
+@Component({
+  selector: 'div.container.my-app',
+  template: `
+    <div class="container text-center">
+      <div class="row"><div class="col-md-12">
+          <div class="page-header"><h1>{{title}}</h1></div>
+          <p class="lead">{{description}}</p>
+      </div></div>
+      <div class="row">
+        <div class="col-md-6" my-child-comp myText="First child component goes here" (onChildMessage)="onChildMessageReceived($event)"></div>      
+        <div class="col-md-6" my-child-comp [myText]="secondComponentText" (onChildMessage)="onChildMessageReceived($event)"></div>         
+      </div>
+      <div class="row"><div class="col-md-12"><div class="well well-sm">         
+            <p>Last message from a child component: <strong>{{lastMessage}}</strong></p>
+      </div></div></div>          
+    </div>`
+})
+export class AppComponent {
+  title: string;
+  description: string;
+  secondComponentText: string;
+  lastMessage: string;
+
+  constructor() {
+    this.title = 'Angular 2 102 example';
+    this.description = 'This is an example for Angular 2 parent and child components with directives';
+    this.secondComponentText = 'Second child component goes here';
+  }
+
+  onChildMessageReceived($event: string) {
+    this.lastMessage = $event;
+  }
+}
